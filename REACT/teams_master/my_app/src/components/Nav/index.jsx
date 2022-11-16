bfe09216fcc48c895384951f2608eefc215fd858
AndreasUglianski <a.uglyansky@gmail.com>
2022-11-16 11:49:20 +0100

    project routingpages started

--- REACT/teams_master/my_app/src/components/Nav/index.jsx
+++ REACT/teams_master/my_app/src/components/Nav/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import s from './index.module.css';
 
 export default function Nav() {
-	return <div>Nav</div>;
+	return (
+		<nav className={s.nav_menu}>
+			<a href='www'>Configurations</a>
+			<a href='www'>Teams</a>
+		</nav>
+	);
 }
---
+++ REACT/teams_master/my_app/src/pages/ConfigurationsPage/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ConfigurationsPage() {
+	return <div>ConfigurationsPage</div>;
+}
