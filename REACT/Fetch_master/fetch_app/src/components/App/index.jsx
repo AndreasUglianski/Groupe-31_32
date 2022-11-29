99ac2df1b47efd78c9c406baf013182003a8e0aa
AndreasUglianski <a.uglyansky@gmail.com>
2022-11-29 08:38:30 +0100

    project fetch requests in react_pages added

---
+++ REACT/Fetch_master/fetch_app/src/components/App/index.jsx
@@ -0,0 +1,38 @@
+import { getUsers } from '../../requests/users';
+import { getProducts } from '../../requests/products';
+import { useState, useEffect } from 'react';
+import { Context } from '../../context';
+import Nav from '../Nav';
+import { Route, Routes } from 'react-router-dom';
+import UserPage from '../../pages/UserPage';
+import ProductsPage from '../../pages/ProductsPage';
+
+function App() {
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		getUsers(setUsers);
+	}, []); // функция вызывается один раз при перезагрузке страницы
+
+	// getUsers(setUsers) - функция вызывается много раз
+
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		getProducts(setProducts);
+	}, []);
+
+	return (
+		<div>
+			<Context.Provider value={{ users, products }}>
+				<Nav />
+				<Routes>
+					<Route path='/UserPage' element={<UserPage />} />
+					<Route path='/ProductsPage' element={<ProductsPage />} />
+				</Routes>
+			</Context.Provider>
+		</div>
+	);
+}
+
+export default App;
