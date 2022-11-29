966da4f338db0555107c01db323ec1effe23ab17
AndreasUglianski <a.uglyansky@gmail.com>
2022-11-29 20:21:05 +0100

    AddPostForm added

--- REACT/Posts_master/posts_app/src/components/App/index.jsx
+++ REACT/Posts_master/posts_app/src/components/App/index.jsx
@@ -1,9 +1,41 @@
 import PostsContainer from '../PostsContainer';
+import { useState } from 'react';
+import { posts_data } from '../../data/posts';
+import { Context } from '../../context';
+import AddPostForm from '../AddPostForm';
 
 function App() {
+	const [posts, setPosts] = useState(posts_data);
+
+	const change_like = (id) => {
+		//найти карточку по id - метод find()
+		const target_card = posts.find((el) => el.id === id);
+
+		// у найденной карточки меняем значение св-ва like на противоположное
+		target_card.like = !target_card.like;
+
+		// отследить изменение состояния (setPosts)
+		setPosts([...posts]);
+	};
+
+	const add_post = (title, text) =>
+		setPosts([
+			...posts,
+			{
+				id: Date.now(),
+				title,
+				text,
+				like: false,
+				comments: [],
+			},
+		]);
+
 	return (
 		<div>
-			<PostsContainer />
+			<Context.Provider value={{ posts, change_like, add_post }}>
+				<AddPostForm />
+				<PostsContainer />
+			</Context.Provider>
 		</div>
 	);
 }
--- REACT/Posts_master/posts_app/src/components/PostsContainer/index.jsx
+++ REACT/Posts_master/posts_app/src/components/PostsContainer/index.jsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../../context';
+import Post from '../Post';
 
 export default function PostsContainer() {
-	return <div>PostsContainer</div>;
+	const { posts } = useContext(Context);
+
+	return (
+		<div>
+			{posts.map((el) => (
+				<Post key={el.id} {...el} />
+			))}
+		</div>
+	);
 }
