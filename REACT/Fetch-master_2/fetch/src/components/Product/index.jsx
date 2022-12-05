7cad7ada3e620d73936e3303f7504ee67149bc54
AndreasUglianski <a.uglyansky@gmail.com>
2022-12-05 10:00:43 +0100

    product images added

--- REACT/Fetch-master_2/fetch/src/components/Product/index.jsx
+++ REACT/Fetch-master_2/fetch/src/components/Product/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import s from './index.module.css';
 
-export default function Product({ title, description, price }) {
+export default function Product({ title, description, price, images }) {
 	return (
 		<div className={s.product_card}>
+			<img src={images[0]} alt='pic' className={s.product_picture} />
 			<p>Title: {title}</p>
 			<p>Description: {description}</p>
 			<p>Price: {price}</p>
