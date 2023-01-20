070336fce386961911b51c4274de4bdb9770eba7
AndreasUglianski <a.uglyansky@gmail.com>
2023-01-20 11:04:41 +0100

    fetch + ProductsCard + ProductsPage added

--- REACT/REDUX/Pre_Project/pre_project/src/components/ProductCard/index.jsx
+++ REACT/REDUX/Pre_Project/pre_project/src/components/ProductCard/index.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import s from './index.module.css';
 
-export default function ProductCard() {
-  return (
-    <div>ProductCard</div>
-  )
+export default function ProductCard({ id, title, price, image }) {
+	return (
+		<div className={s.product_card}>
+			<div>
+				<img src={image} alt={title} />
+				<button>Add to cart</button>
+			</div>
+
+			<p>{title}</p>
+		</div>
+	);
 }
