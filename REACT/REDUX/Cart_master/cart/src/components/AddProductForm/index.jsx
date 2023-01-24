97ab2eceae2955778377fa2dfe1c6ff3f8d7f578
AndreasUglianski <a.uglyansky@gmail.com>
2023-01-24 07:43:01 +0100

    cart master started

---
+++ REACT/REDUX/Cart_master/cart/src/components/AddProductForm/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { addProduct } from '../../store/reducers/productReducer';
+import { useDispatch } from 'react-redux';
+
+
+export default function AddProductForm() {
+
+    const dispatch =useDispatch()
+
+    const submit = event => {
+        event.preventDefault()
+        const { title, price } = event.target;
+        dispatch(addProduct({
+            id: Date.now(),
+            title: title.value,
+            price: price.value
+        }))
+        title.value = ''
+        price.value = ''
+    }
+
+	return (
+		<form onSubmit={submit}>
+			<input type='text' name='title' placeholder='Title' />
+			<input type='number' name='price' placeholder='Price' />
+			<button>Add product</button>
+		</form>
+	);
+}
---
+++ REACT/REDUX/Cart_master/cart/src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux'
+import { cartReducer } from './reducers/cartReducer'
+import { productReducer } from './reducers/productReducer'
+
+
+const rootReducer = combineReducers({
+    products: productReducer,
+    cart: cartReducer
+})
+
+export const store = createStore(rootReducer)
