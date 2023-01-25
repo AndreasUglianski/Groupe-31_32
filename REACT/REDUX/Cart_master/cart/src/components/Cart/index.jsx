dbd386579de9e7615a400fd36ce546d7c495825d
AndreasUglianski <a.uglyansky@gmail.com>
2023-01-25 13:56:47 +0100

    cart finished

--- REACT/REDUX/Cart_master/cart/src/components/Cart/index.jsx
+++ REACT/REDUX/Cart_master/cart/src/components/Cart/index.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import CartCard from '../CartCard';
+import { clearCart } from '../../store/reducers/cartReducer';
 
 export default function Cart() {
+	const cart = useSelector((state) => state.cart);
+	const dispatch = useDispatch();
 
-    const cart = useSelector(state => state.cart)
-    
-    console.log(cart)
+	const clear_cart = () => dispatch(clearCart());
 
-  return (
-    <div>Cart</div>
-  )
+	const total = cart.reduce(
+		(prev, { price, count }) => prev + price * count,
+		0
+	);
+
+	return (
+		<div>
+			<p>Cart:</p>
+			<div>
+				{cart.map((el) => (
+					<CartCard key={el.id} {...el} />
+				))}
+			</div>
+			<div>
+				<p>Total: {total}</p>
+				<button onClick={clear_cart}>Clear cart</button>
+			</div>
+		</div>
+	);
 }
