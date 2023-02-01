a13c03852340ec81fb960ef24be63ca6c6a259e6
AndreasUglianski <a.uglyansky@gmail.com>
2023-02-01 16:46:09 +0100

    Header done

---
+++ REACT/React-seit_template/seit/src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from '../Button';
+import Nav from '../Nav';
+import s from './index.module.css';
+import logo from './media/logo_pic.png';
+import { BsGlobe2 } from 'react-icons/bs';
+
+export default function Header() {
+	return (
+		<header className={s.main_header}>
+			<div>
+				<Nav />
+				<img src={logo} alt='logo' />
+				<div className={s.info_block}>
+					<p>
+						Sales:
+						<bp />
+						+1 (800) 315-5939
+					</p>
+					<BsGlobe2 className={s.glob_icon} />
+					<p>Serch: </p>
+					<p>Log in: </p>
+				</div>
+				<Button>Sign up</Button>
+			</div>
+		</header>
+	);
+}
