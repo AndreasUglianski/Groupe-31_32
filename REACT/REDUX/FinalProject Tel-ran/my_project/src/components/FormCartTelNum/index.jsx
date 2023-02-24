1ea5d73ff5c86319e48083979025de9c2dbdc482
AndreasUglianski <a.uglyansky@gmail.com>
2023-02-24 11:34:23 +0100

    styles updts

--- REACT/REDUX/FinalProject Tel-ran/my_project/src/components/FormCartTelNum/index.jsx
+++ REACT/REDUX/FinalProject Tel-ran/my_project/src/components/FormCartTelNum/index.jsx
@@ -3,26 +3,39 @@ import s from './index.module.css'
 import { useForm } from 'react-hook-form'
 
 export default function FormCartTelNum() {
-	const { register, handleSubmit } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm();
 
-	const submit = (data) => console.log(data);
+	const submit = (data) => {
+		console.log(data);
+		reset();
+	};
 
-	const phoneRegister = register('phone');
+	const phoneRegister = register('phone', {
+		required: '***The field "phone number" is requared***',
+		pattern: {
+			value: /^(\+49|0049|0)[1-9]\d{1,14}$/,
+			message: 'Must be in the format: +49 123456789***',
+		},
+	});
 
 	return (
 		<form onSubmit={handleSubmit(submit)} className={s.cart_form}>
 			<input
 				type='tel'
 				name='phone'
-				defaultValue={'+49 '}
+				// defaultValue={'+49 '}
 				title='phone'
+				placeholder='Enter your phone number'
 				maxLength={14}
-				// pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}-[0-9]{4}'
-				required
 				{...phoneRegister}
 			/>
-			<button>Order</button>
+			{errors?.phone && <p>{errors?.phone?.message}</p>}
+			<button>Ordder</button>
 		</form>
 	);
 }
-
