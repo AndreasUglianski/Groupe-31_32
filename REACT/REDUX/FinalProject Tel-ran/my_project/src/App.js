d0b4248796159af818586b61e021dd4dc8724966
AndreasUglianski <a.uglyansky@gmail.com>
2023-03-03 12:55:09 +0100

    localstorege added

--- REACT/REDUX/FinalProject Tel-ran/my_project/src/App.js
+++ REACT/REDUX/FinalProject Tel-ran/my_project/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
@@ -8,12 +10,24 @@ import CartPage from './pages/CartPage';
 import ProductsPage from './pages/ProductsPage';
 import PromoContainer from './components/PromoContainer';
 import DiscountCoupon from './components/DiscountCoupon';
-
-
-
-
+import { localStoreCard } from './store/reducers/cart';
 
 function App() {
+
+	const dispatch = useDispatch();
+
+	const cart = useSelector((state) => state.cart);
+
+	useEffect(() => {
+		const cartLocal = JSON.parse(localStorage.getItem('cart'));
+		if (cartLocal && cartLocal.length) 
+	dispatch(localStoreCard(cartLocal));
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart));
+	}, [cart]);
+
 	return (
 		<div>
 			<Routes>
--- REACT/REDUX/FinalProject Tel-ran/my_project/src/store/reducers/cart.js
+++ REACT/REDUX/FinalProject Tel-ran/my_project/src/store/reducers/cart.js
@@ -1,4 +1,3 @@
-
 const defaultState = [];
 
 const ADD_TO_CART = 'ADD_TO_CART';
@@ -6,12 +5,17 @@ const CLEAR_CART = 'CLEAR_CART';
 const DELETE_PRODUCT = 'DELETE_PRODUCT';
 const INCREMENT_COUNT = 'INCREMENT_COUNT';
 const DECREMENT_COUNT = 'DECREMENT_COUNT';
+const LOCAL_STORE_CARD = 'LOCAL_STORE_CARD';
 
 export const addToCart = (payload) => ({ type: ADD_TO_CART, payload });
 export const clearCart = () => ({ type: CLEAR_CART });
 export const deleteProduct = (payload) => ({ type: DELETE_PRODUCT, payload });
 export const incrementCount = (payload) => ({ type: INCREMENT_COUNT, payload });
 export const decrementCount = (payload) => ({ type: DECREMENT_COUNT, payload });
+export const localStoreCard = (payload) => ({
+	type: LOCAL_STORE_CARD,
+	payload,
+});
 
 const checkProduct = (state, payload) => {
 	const productInState = state.find((el) => el.id === payload.id);
@@ -31,7 +35,6 @@ const checkProduct = (state, payload) => {
 
 export const cartReducer = (state = defaultState, action) => {
 	if (action.type === ADD_TO_CART) {
-		
 		return checkProduct(state, action.payload);
 	} else if (action.type === CLEAR_CART) {
 		return defaultState;
@@ -46,6 +49,9 @@ export const cartReducer = (state = defaultState, action) => {
 			? (state = state.filter((el) => el.id !== action.payload))
 			: target_card.count--;
 		return [...state];
+	} else if (action.type === LOCAL_STORE_CARD) {
+		const defaultState = action.payload;
+		return [...defaultState];
 	} else {
 		return state;
 	}
